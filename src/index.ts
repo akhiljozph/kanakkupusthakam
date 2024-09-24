4b4b766411c70ce653af66610873e265d9de429b
akhil.jozph <ajpulluvelil@gmail.com>
2024-09-24 16:04:58 +0530

    EX-2 - Added development package supports such as dotenv, pino, pino-pretty, and nodemon.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import Koa from "koa";
+import 'dotenv/config'
+
+import logger from "./common/logger/logger";
 
 const app = new Koa();
+const PORT = process.env.PORT || 3000;
 
 app.use(async (ctx) => {
     ctx.body = "Hello World";
 });
 
-app.listen(3000);
+app.listen(PORT, () => {
+    logger.info(`ex-tracker is running on port ${PORT}`)
+});
