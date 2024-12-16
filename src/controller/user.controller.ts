9e254442727e3bf269f14bafbab0bb14e02a6953
akhil.jozph <ajpulluvelil@gmail.com>
2024-12-16 14:41:59 +0530

    EX-9 - Updates for create user api.

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,24 +1,27 @@
 import { Response, Request } from "koa";
 
-import log from "../common/logger/logger";
-import { HttpCodesEnum } from "../common/enum/enum";
 import logger from "../common/logger/logger";
+import { HttpCodesEnum } from "../common/enum/enum";
 
 export function createUserHandler(request: Request, response: Response) {
-    log.info(`CreateUserHandler: ${request.url} has requested!`);
+    logger.info(`CreateUserHandler: ${request.url} has requested!`);
 
     let userDetails = request.body;
     let responseStatus: number = HttpCodesEnum.NO_CONTENT;
+    let responseMessage: string = "Request pending!";
 
     try {
-        if (userDetails) {
-            responseStatus = HttpCodesEnum.CREATED;
+        if (!!userDetails) {
+            throw({message: "Request body is undefined!"})
         } else {
-            throw("Request body is undefined!")
+            responseStatus = HttpCodesEnum.CREATED;
+            responseMessage = "User has been created!"
         }
-    } catch (error) {
+    } catch (error: any) {
         responseStatus = HttpCodesEnum.BAD_REQUEST;
+        responseMessage = error.message;
     } finally {
         response.status = responseStatus;
+        response.message = responseMessage;
     }
 }
