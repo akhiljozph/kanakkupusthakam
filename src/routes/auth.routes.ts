0b7ec98c9bb01f9bea9e4f6990b40c24d3b0c2f8
akhiljozph <ajpulluvelil@gmail.com>
2025-02-04 22:14:18 +0530

    EX-17 - Modulrized routes.

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -2,7 +2,19 @@ import Router from "koa-router";
 
 const authRoutes = new Router();
 
-authRoutes.get('/hello', async(ctx) => {
+authRoutes.post('/registration', async(ctx) => {
+    ctx.body = { message: "Hello from authRoutes."}
+});
+
+authRoutes.post('/create/password', async(ctx) => {
+    ctx.body = { message: "Hello from authRoutes."}
+});
+
+authRoutes.post('/create/password', async(ctx) => {
+    ctx.body = { message: "Hello from authRoutes."}
+});
+
+authRoutes.get('/login', async(ctx) => {
     ctx.body = { message: "Hello from authRoutes."}
 });
 
