c44c6e17bc7df12fb9857e29818edbba22ee03bb
akhiljozph <ajpulluvelil@gmail.com>
2025-11-29 20:50:11 +0530

    EX-25 - Reinitialized with nest.js

--- src/routes/filter.routes.ts
+++
@@ -1,9 +0,0 @@
-import Router from "koa-router";
-
-const filterRoutes = new Router();
-
-filterRoutes.get('/hello', async(ctx) => {
-    ctx.body = { message: "Hello from filterRoutes."}
-});
-
-export default filterRoutes;
