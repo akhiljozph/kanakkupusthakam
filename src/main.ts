50b1afafe391391adc5ab6e11071cd4ae2d16878
akhiljozph <ajpulluvelil@gmail.com>
2025-11-30 18:32:45 +0530

    EX-29 - Added swagger for API documentation.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
@@ -12,6 +13,15 @@ async function bootstrap() {
     type: VersioningType.URI,
     defaultVersion: '1',
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Ex-tracker APIs')
+    .setDescription('API details for ex-tracker application.')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
