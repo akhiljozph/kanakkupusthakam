ad7c89317d1de6a4f43e5cccecefb85091dba4c1
Akhil Joseph <ajpulluvelil@gmail.com>
2026-02-03 12:21:20 +0530

    KANK-8 - Implementation of custom hook in ui.

---
+++ kanakk-ui/src/hooks/useFetch.ts
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+
+const useFetch = () => {
+
+    useEffect(() => {
+        try {
+
+        } catch (error) {
+
+        } finally {
+
+        }
+    });
+
+}
+export default useFetch;
