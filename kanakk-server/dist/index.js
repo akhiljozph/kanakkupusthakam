a5ce26edf85303207f5e45252fa8fff55355d3d3
akhiljozph <ajpulluvelil@gmail.com>
2026-02-04 23:26:35 +0530

    KANK-9 - Kanakk-server implementations.

--- kanakk-server/dist/index.js
+++ kanakk-server/dist/index.js
@@ -1,10 +1,10 @@
-import * as Koa from "koa";
-import * as Router from "koa-router";
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Koa = require("koa");
+const Router = require("koa-router");
 const app = new Koa();
 const router = new Router();
-
 app.listen(3000, () => {
     console.info("Hey AK! I am listening for your commands, ping me at 3000!");
     console.info("AK! it was a horrible experience. But don't worry, I found your data!");
-})
+});
