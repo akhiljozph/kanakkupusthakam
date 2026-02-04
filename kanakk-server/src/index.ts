063ede7dabe957041dfc09e8f0c6a8387c8cc79c
akhiljozph <ajpulluvelil@gmail.com>
2026-02-04 23:47:13 +0530

    KANK-9 - Kanakk-server implementations.

--- kanakk-server/src/index.ts
+++ kanakk-server/src/index.ts
@@ -4,7 +4,11 @@ import * as Router from "koa-router";
 const app = new Koa();
 const router = new Router();
 
+app.use(async ctx => {
+    ctx.body = "Hey, I'm here! Need help? Send the magic word with some tips."
+})
+
 app.listen(3000, () => {
     console.info("Hey AK! I am listening for your commands, ping me at 3000!");
-    console.info("AK! it was a horrible experience. But don't worry, I found your data!");
-})
+    console.info("AK! It was a horrible experience, but don't worry—I found your data!");
+});
