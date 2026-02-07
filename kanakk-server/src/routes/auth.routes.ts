7a5d4120d7c65407f009a2b171bf1caafe566e33
Akhil Joseph <45339469+akhiljozph@users.noreply.github.com>
2026-02-07 23:15:33 +0530

    KANK-9 - New route added in the server app.

--- kanakk-server/src/routes/auth.routes.ts
+++ kanakk-server/src/routes/auth.routes.ts
@@ -3,7 +3,9 @@ import Router from '@koa/router';
 const router = new Router();
 
 router.post('/sign-in', (ctx) => {
-    ctx.body = { status: 2000, body: {} }
+    ctx.body = { status: 200, body: {} }
 });
+
+router.post('/signup', (ctx) => { ctx.body = { status: 200, body: {} } });
 
 export default router;
