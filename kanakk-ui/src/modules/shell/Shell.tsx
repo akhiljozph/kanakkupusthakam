9f80fe856bf6f7f401eca708bdfe5bfd2cd2de32
Akhil Joseph <ajpulluvelil@gmail.com>
2026-02-13 21:59:10 +0530

    KANK-13 -  Added shell component and added axios security breach fix.

---
+++ kanakk-ui/src/modules/shell/Shell.tsx
@@ -0,0 +1,10 @@
+import type { ShellProps } from "./Shell.types";
+
+import "./Shell.scss";
+
+const Shell = (props: ShellProps) => {
+    return (
+        <>Dashboard works!</>
+    )
+}
+export default Shell;
