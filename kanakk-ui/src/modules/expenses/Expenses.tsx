cbd3b80768d4111f8a8f847c326f9754be64f382
akhiljozph <ajpulluvelil@gmail.com>
2026-02-15 22:28:05 +0530

    KANK-15 - Added new expenses module.

---
+++ kanakk-ui/src/modules/expenses/Expenses.tsx
@@ -0,0 +1,12 @@
+import type { ExpensesProps } from "./Expenses.types";
+
+import "./Expenses.scss";
+
+const Expenses = (props: ExpensesProps) => {
+    return (
+        <>
+            expenses works!
+        </>
+    )
+}
+export default Expenses;
