82bac33bc3b3ae433429f9753b8cce5cad5cb476
Akhil Joseph <ajpulluvelil@gmail.com>
2026-02-19 18:07:22 +0530

    KANK-19 - Implementation of input validation for APIs.

--- kanakk-server/src/routes/auth.routes.ts
+++ kanakk-server/src/routes/auth.routes.ts
@@ -2,10 +2,28 @@ import Router from '@koa/router';
 
 const router = new Router();
 
+interface usersInputValidationSchema {
+    firstName: string;
+    lastName: string;
+    email: string;
+    mobileNumber: string;
+    password: string;
+    country: string;
+    state: string;
+    cityOrDistrict: string;
+    locality: string;
+    landmark: string;
+    pinCode: string;
+    apartment: string;
+    dateOfBirth: string;
+}
+
 router.post('/sign-in', (ctx) => {
     ctx.body = { status: 200, body: {} }
 });
 
-router.post('/signup', (ctx) => { ctx.body = { status: 200, body: {} } });
+router.post('/signup', (ctx) => { 
+    ctx.body = { status: 200, body: {} } 
+});
 
 export default router;
