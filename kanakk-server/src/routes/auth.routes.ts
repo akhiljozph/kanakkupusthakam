fa9018c32fd566362c94315f41e8bd7692cf0c7a
akhiljozph <ajpulluvelil@gmail.com>
2026-02-20 23:56:41 +0530

    KANK-19 - Implementation of validation schemas.

--- kanakk-server/src/routes/auth.routes.ts
+++ kanakk-server/src/routes/auth.routes.ts
@@ -1,29 +1,25 @@
 import Router from '@koa/router';
 
+import signUpSchema from '../schemas/input-schemas/sign-up.schema';
+
 const router = new Router();
 
-interface usersInputValidationSchema {
-    firstName: string;
-    lastName: string;
-    email: string;
-    mobileNumber: string;
-    password: string;
-    country: string;
-    state: string;
-    cityOrDistrict: string;
-    locality: string;
-    landmark: string;
-    pinCode: string;
-    apartment: string;
-    dateOfBirth: string;
-}
 
 router.post('/sign-in', (ctx) => {
+
     ctx.body = { status: 200, body: {} }
 });
 
-router.post('/signup', (ctx) => { 
-    ctx.body = { status: 200, body: {} } 
+router.post('/signup', (ctx) => {
+    const parsedBody = signUpSchema.safeParse(ctx.request.body);
+
+    if (!parsedBody.success) {
+        ctx.status = 400;
+        ctx.body = { status: 400, errors: parsedBody.error.flatten().fieldErrors };
+        return;
+    }
+
+    ctx.body = { status: 200, body: {} }
 });
 
 export default router;
